c04165ecc533cb4abca95f1c25c60e6ca648f4a2
KevinPNguy01 <kevinpnguy01@gmail.com>
2024-05-08 22:31:12 -0700

    InvertedIndex saves to json

--- src/inverted_index.py
+++ src/inverted_index.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from posting import Posting
 from typing import Dict, List
+import json
 
 class InvertedIndex:
     def __init__(self, source: Path) -> None:
@@ -13,3 +14,8 @@ class InvertedIndex:
             for token, posting in Posting.get_postings(file, id).items():
                 self.postings.setdefault(token, [])
                 self.postings.get(token).append(posting)
+                
+    def save_to_file(self, dest: Path) -> None:
+        index: Dict[str, List[dict]] = {}
+        with open(dest, "w") as file:
+            json.dump({token: [{"id": p.id, "url": p.url, "tf_idf": p.tf_idf} for p in postings] for token, postings in self.postings.items()}, file, indent=4)
--- src/main.py
+++ src/main.py
@@ -3,3 +3,4 @@ from pathlib import Path
 
 if __name__ == "__main__":
     index = InvertedIndex(Path("asterix_ics_uci_edu"))
+    index.save_to_file("index.json")
