7db51c9d78de4ace2eefeae8495c7c310a167ee3
KevinPNguy01 <kevinpnguy01@gmail.com>
2024-05-08 22:17:40 -0700

    Postings are added as files are read

--- src/inverted_index.py
+++ src/inverted_index.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 from posting import Posting
-from typing import Dict
+from typing import Dict, List
 
 class InvertedIndex:
     def __init__(self, source: Path) -> None:
         # Map of tokens to lists of Postings.
-        self.token_map: Dict[str, Posting] = {}
+        self.postings: Dict[str, List[Posting]] = {}
         
         # Iterate through all json files in source directory.
         for id, file in enumerate(source.rglob("*.json")):
-            Posting.get_postings(file, id)
+            # Add all postings from that file to this index's own dict.
+            for token, posting in Posting.get_postings(file, id).items():
+                self.postings.setdefault(token, [])
+                self.postings.get(token).append(posting)
--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@ from inverted_index import InvertedIndex
 from pathlib import Path
 
 if __name__ == "__main__":
-    index = InvertedIndex(Path("DEV"))
+    index = InvertedIndex(Path("asterix_ics_uci_edu"))
