601a1fd84ece82a685cb55218cb8551682efc67c
KevinPNguy01 <kevinpnguy01@gmail.com>
2024-05-08 21:39:56 -0700

    Added InvertedIndex class

---
+++ src/inverted_index.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from posting import Posting
+from typing import Dict
+
+class InvertedIndex:
+    def __init__(self, source: Path) -> None:
+        # Map of tokens to lists of Postings.
+        self.token_map: Dict[str, Posting] = {}
+        
+        # Iterate through all json files in source directory.
+        for file in source.rglob("*.json"):
+            print(file.name)
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from inverted_index import InvertedIndex
+from pathlib import Path
+
+if __name__ == "__main__":
+    index = InvertedIndex(Path("DEV"))
