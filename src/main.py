b01191b1c30f0406e6f06bef3e884c39db280cfa
KevinPNguy01 <kevinpnguy01@gmail.com>
2024-05-08 22:46:57 -0700

    Added print status for postings

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from inverted_index import InvertedIndex
 from pathlib import Path
 
 if __name__ == "__main__":
-    index = InvertedIndex(Path("asterix_ics_uci_edu"))
+    index = InvertedIndex(Path("DEV"))
     index.save_to_file("index.json")
--- src/posting.py
+++ src/posting.py
@@ -9,6 +9,7 @@ class Posting:
         # Returns a dict of tokens to postings for this file.
         with open(file_path) as file:
             data = json.load(file)
+            print(f"{id} - {data['url']}")
             
             # Extract text from webpage content.
             text = extract_text(data["content"])
