3883bfc3087e6caa1824890890c37fb8c36e8771
Tyler Tran <tylert6@uci.edu>
2024-05-17 17:56:17 -0700

    created report pdf and modified search_engine such that it has a UI and sorts links by tf_idf

--- src/search_engine.py
+++ src/search_engine.py
@@ -1,21 +1,43 @@
+from collections import defaultdict
 import json
+import streamlit as st 
 
 def main():
     try:
         with open("index.json", "r") as f:
             inverted_index = json.load(f)
-        while True:
-            line = input("Enter a query: ")
-            tokens = line.split()
-            token_postings = [inverted_index.get(token, []) for token in tokens]
-            least_frequent = min(token_postings, key=len)
-            results = []
-            for document in least_frequent:
-                id = document["id"]
-                if all([any([posting["id"] == id for posting in postings]) for postings in token_postings]):
-                    results.append(document)
-            for index, posting in enumerate(results):
-                print(f"{index}. {posting["url"]}")
+
+        st.title("Search Engine")
+        user_input = st.text_input("Enter a query: ")
+
+        if st.button("Search"):
+            if user_input:
+                tokens = user_input.split()
+                token_postings = [inverted_index.get(token, []) for token in tokens] # just a list of lists of postings
+                least_frequent = min(token_postings, key=len) # finds the list in the postings with the smallest len 
+                results = []
+
+                doc_tf_idf = defaultdict(float)
+                for document in least_frequent:
+                    id = document["id"]
+                    # checks if the website / id is present in all other lists in token_postings 
+                    # where each list in the token_posting is another token 
+                    if all([any([posting["id"] == id for posting in postings]) for postings in token_postings]):
+                        results.append(document)
+
+                        for postings in token_postings:
+                            for posting in postings:
+                                if posting["id"] == id:
+                                    # add the tf_idf's of the tokens 
+                                    doc_tf_idf[id] += posting["tf_idf"]
+
+                for document in results:
+                    document["tf_idf"] = doc_tf_idf[document["id"]]
+
+                for index, posting in enumerate(sorted(results[:5], key= lambda x: x["tf_idf"], reverse = True), start = 1):
+                    st.write(f"{index}. {posting['url']}")
+
+            
     except KeyboardInterrupt:
         print("\nExit successful.")
 
