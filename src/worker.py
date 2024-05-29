815858e3f78df76a48a9b996e1e01bf91b5a0345
KevinPNguy01 <kevinpnguy01@gmail.com>
2024-05-29 13:42:50 -0700

    Cleaned up worker.py

--- src/worker.py
+++ src/worker.py
@@ -22,20 +22,48 @@ def is_valid_html(content: str) -> bool:
 class Worker:
 	def __init__(self, worker_id: int, folder: Path, q_in: Queue, q_out: Queue, running: ctypes.c_int):
 		self.worker_id = worker_id
-		self.folder = folder
-		self.q_in = q_in
-		self.q_out = q_out
+		self.folder = folder		# Folder to create partial indices in.
+		self.q_in = q_in			# Queue to receive documents to process.
+		self.q_out = q_out			# Queue to send back processed documents.
 		self.running = running
 
 		self.postings: Dict[str, List[Posting]] = {}    # Map of tokens to lists of Postings that contain that token.
 		self.posting_count: int = 0						# The current number of postings.
 		self.index_count = 0							# The current number of partial indices.
+
+		# Count the number of partial indices associated with this worker id.
 		for file in os.listdir(self.folder):
 			if file.startswith(f"w{self.worker_id}"):
 				self.index_count += 1
 
+	def __call__(self):
+		""" Called when the worker process is started. """
+		signal.signal(signal.SIGINT, signal.SIG_IGN)
+		try:
+			self.run()
+		except KeyboardInterrupt:
+			pass
+		self.exit()
+
+	def run(self):
+		# Run until the main thread signals to stop, or if a None Value is encountered.
+		while self.running.value and (doc_and_id := self.q_in.get()):
+			self.process_document(*doc_and_id)
+
+			# If there are more than 100,000 postings stored in memory, write them to a partial index.
+			if self.posting_count > 100000:
+				self.create_partial_index()
+
+	def exit(self):
+		""" Called when the worker exits due to reaching the end of the queue or from KeyboardInterrupt. """
+
+		self.create_partial_index()		# Write the rest of the postings to a partial index.
+		self.q_out.put(None)			# Signal to the main process that this worker is done.
+		print(f"Worker {self.worker_id} exited.")
+
 	def create_partial_index(self) -> None:
 		""" Write the tokens and postings stored in memory into a partial index. """
+
 		with open(f"{self.folder}/w{self.worker_id}-i{self.index_count}.dat", "w") as index:
 			for token, postings in sorted(self.postings.items()):
 				# Each line of the index is of the form: 
@@ -46,46 +74,27 @@ class Worker:
 				# The token "research" is found in documents 0, 4, 7 with tf-idfs of 3, 2, 3 respectively.
 				# The token "computer" is found in documents 0, 3, 4 with tf-idfs of 12, 8, 9.
 				line = f"{token}:" + ";".join(f"{p.id},{p.tf_idf}" for p in postings) + "\n"
-				try:
-					index.write(line)
-				except:
-					print(line)
+				index.write(line)
+
 		# Update relevant variables.
 		self.postings = {}
 		self.posting_count = 0
 		self.index_count += 1
 
-	def read_file(self, file_path: Path, id: int) -> None:
+	def process_document(self, file_path: Path, id: int) -> None:
+		""" Processing the given document, extracting the postings from it. """
+
+		# Load json file for this document quickly.
 		with open(file_path, encoding="utf-8") as f:
 			page = decoder.decode(f.read())
+
 		# Skip file if it contains invalid html.
 		if not is_valid_html(page.content):
 			return
 
-		# Add all postings from that file to this index's own dict.
+		# Add all postings from that file to this worker's own dict.
 		for token, posting in Posting.get_postings(page.content, id).items():
 			self.postings.setdefault(token, []).append(posting)
 			self.posting_count += 1
-		print(f"Worker {self.worker_id} - {id} - {file_path}")
 		self.q_out.put((file_path, id))
-
-	def run(self):
-		while self.running.value and (doc_and_id := self.q_in.get()):
-			self.read_file(*doc_and_id)
-
-			# If there are more than 100,000 postings stored in memory, write them to a partial index.
-			if self.posting_count > 100000:
-				self.create_partial_index()
-
-	def __call__(self):
-		signal.signal(signal.SIGINT, signal.SIG_IGN)
-		try:
-			self.run()
-		except KeyboardInterrupt:
-			pass
-		self.exit()
-
-	def exit(self):
-		self.create_partial_index()
-		self.q_out.put(None)			# Signal to the main process that this worker is done.
-		print(f"Worker {self.worker_id} exited.")
+		print(f"Worker {self.worker_id} - {id} - {file_path}")
